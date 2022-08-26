24fd1d1506632d0014852c5848245bd12ad99713
Ntokozo Gumede <ntandontokozo5@gmail.com>
2022-08-26 21:19:44 +0200

    finished build

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const form = document.getElementById('generate-form')
+const qr = document.getElementById('qrcode')
+
+const onGenerateSubmit = (e) =>{
+    e.preventDefault();
+    clearUI();
+
+
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+
+    if(url===''){
+        alert('Please enter a URL!')
+    }else{
+        showSpinner();
+
+        setTimeout(() =>{
+            hideSpinner();
+            generateCode(url, size)
+
+            setTimeout(() =>{
+                const saveUrl = qr.querySelector('img').src;
+                createBtnSave(saveUrl)
+            }, 50);
+        },1000)
+    }
+};
+
+    const generateCode = (url, size) =>{
+        const qrcode = new QRCode('qrcode',{
+            text: url,
+            with: size,
+            height:size
+        });
+    }
+    const showSpinner = () => {
+        document.getElementById('spinner').style.display = 'block';
+    };
+    const  hideSpinner = () =>{
+        document.getElementById('spinner').style.display='none';
+    };
+
+    const clearUI = () =>{
+        qr.innerHTML = '';
+        const saveLink = document.getElementById('save-link');
+        if(saveLink){
+            saveLink.remove();
+        }
+    };
+
+    const createBtnSave = (saveUrl) =>{
+        const link = document.createElement('a');
+        link.id = 'save-link';
+        link.classList = 'bg-green-500 hover:bg-dark text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+        link.href = saveUrl;
+        link.download = 'qrcode';
+        link.innerHTML = 'Save Image';
+        document.getElementById('generated').appendChild(link);
+    };
+    hideSpinner();
+
+form.addEventListener('submit', onGenerateSubmit);
